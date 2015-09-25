64f2e7d4203322f24b2cd52877e91623288d7684
Niharika Narayan <niharikanarayan1995@gmail.com>
2015-09-25 17:15:36 +0530

    Generic OAuth2 implementation.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-var _ = require('underscore');
-var google = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
 var Parse = require('parse/node');
+var Request = require('request');
+var querystring = require('querystring')
 
 var app = express();
 
@@ -25,14 +24,10 @@ var CLIENT_ID = '703621348399-tci3q5mvsajmjlhbdmb3510ja8c77mi5.apps.googleuserco
 var CLIENT_SECRET = 'MHcQVB1I0LlWzQgoffnd6rRP';
 var REDIRECT_URL = 'https://floating-forest-3059.herokuapp.com/oauthCallback';
 
-var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 var scopes = [
   'https://www.googleapis.com/auth/plus.me',
   'https://www.googleapis.com/auth/drive'
 ];
-var auth_url = oauth2Client.generateAuthUrl({
-  scope: scopes
-});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -42,67 +37,46 @@ app.get('/', function(request, response) {
 });
 
 app.get('/authorize', function(request, response) {
+  auth_url = 'https://accounts.google.com/o/oauth2/auth' + '?' + querystring.stringify({
+    'scope': scopes[0] + ' ' + scopes[1],
+    'response_type': 'code',
+    'client_id': CLIENT_ID,
+    'access_type': 'offline',
+    'approval_prompt': 'force',
+    'redirect_uri': REDIRECT_URL
+  });
   response.redirect(auth_url);
 });
 
 app.get('/oauthCallback', function(request, response) {
-  var code = request.query.code;
-  oauth2Client.getToken(code, function(err, tokens) {
-    if(!err) {
-      oauth2Client.setCredentials(tokens);
-      var token = new TokenStorage();
-      token.setACL(restrictedAcl);
-      Parse.Cloud.useMasterKey();
-      token.save(null, { useMasterKey: true });
-      var plus = google.plus('v1');
-      plus.people.get({ userId: 'me' });
-      var user = new Parse.User();
+  if(request.query && request.query['code']) {
+    var code = request.query.code;
+  }
+  else {
+    return response.redirect(401, '/');
+  }
+  Request.post({
+    url:'https://www.googleapis.com/oauth2/v3/token',
+    form: {
+      'code': code,
+      'client_id': CLIENT_ID,
+      'client_secret': CLIENT_SECRET,
+      'redirect_uri': REDIRECT_URL,
+      'access_type': 'offline',
+      'grant_type': 'authorization_code'
     }
-    return response.send('Error fetching oauth data');
+  }, function(err, httpResponse, body) {
+    if (!err && httpResponse.statusCode == 200) {
+      console.log(httpResponse.statusCode);
+      var token = JSON.parse(body);
+      return response.send(token);
+    }
+    else {
+      console.log(err);
+      return response.send(err);
+    }
   });
 });
-
-// app.get('/oauthCallback2', function(request, response) {
-//   Parse.Promise.as().then(function() {
-//     /** 
-//      * Check if we get an authorization code as parameter.
-//      */
-//     return request.query.code;
-//   }).then(function (code) {
-//     /*
-//      * Exchange authorization code for an
-//      * access token and a refresh token (optional).
-//      */
-//     return Parse.Promise.as().then(function() {
-//       return oauth2Client.getToken(code);
-//     }
-//   }).then(function(err, tokens) {
-//     /*
-//      * Process the response from Google.
-//      * Save the access token in the client and under TokenStorage class.
-//      */
-//     oauth2Client.setCredentials(tokens);
-//     var token = new TokenStorage();
-//     // No public access.
-//     token.setACL(restrictedAcl);
-//     Parse.Cloud.useMasterKey();
-//     return token.save(null, { useMasterKey: true });
-//   }).then(function() {
-//     /*
-//      * Query Google+ profile.
-//      */ 
-//     var plus = google.plus('v1');
-//     return Parse.Promise.as(plus.people.get({ userId: 'me' }));
-//   }).then(function() {
-//     var user = new Parse.User();
-//     user.set({
-
-//     })
-//   }, function(error) {
-//     // If there's an error storing the request, render the error page.
-//     response.send('Failed to save auth request.');
-//   });
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
